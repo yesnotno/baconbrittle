354655290c847a8b92c66596b795d4fc56a4b9d0
yesnotnode <yesnotnode@gmail.com>
2021-11-29 21:34:44 -0600

    SEO Changes & Minor Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 
 import Landing from "../components/landing/landing"
 
-const IndexPage = () => <Landing />
+const IndexPage = () => (
+  <>
+    <Seo title="Bacon Brittle: Home" />
+    <Landing />
+  </>
+)
 
 export default IndexPage
