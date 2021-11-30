885b133cd0ec2fcfd51726bb6ccbc006b4d9a58e
yesnotnode <yesnotnode@gmail.com>
2021-11-29 22:25:53 -0600

    Responsive Page Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from "react"
 
 import Seo from "../components/seo"
+import Layout from "../components/layout"
 import Landing from "../components/landing/landing"
 
 const IndexPage = () => (
   <>
-    <Seo title="Bacon Brittle: Home" />
-    <Landing />
+    <Layout>
+      <Seo title="Bacon Brittle: Home" />
+      <Landing />
+    </Layout>
   </>
 )
 
